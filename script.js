fb67a195f426ed3810c1e7ffe3ae4f24ec61bd04
Pandeysuryansh <167824825+Pandeysuryansh@users.noreply.github.com>
2025-07-01 22:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 fuction daysOfAYear(year){
 	year = parseInt(year);
 	if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
-		return 366; 
+		return 366;
 	}
 	else{
 		return 365;
 	}
 }
 
-let inputYear = prompt("Enter a year between 1 and 9999:");
-alert(daysOfAYear(inputYear));
+console.log(daysOfAYear(2022));
+console.log(daysOfAYear(2024));
+console.log(daysOfAYear(1900));
+console.log(daysOfAYear(2000));
 
 
