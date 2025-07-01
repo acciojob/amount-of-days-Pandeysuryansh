2758d7aa8d9362ac9d7b97aacb5b12f46a1e822c
Pandeysuryansh <167824825+Pandeysuryansh@users.noreply.github.com>
2025-07-01 22:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 fuction daysOfAYear(year){
-	year = parseInt(year);
+	year = Number(year);
 	if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
 		return 366;
 	}
